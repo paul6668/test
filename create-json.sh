1f01ab21b466b574c2ab306357eaf94df8f65d3f
paul6668 <105109093+paul6668@users.noreply.github.com>
2023-11-29 11:16:12 +0800

    Update create-json.sh

--- create-json.sh
+++ create-json.sh
@@ -61,3 +61,44 @@ for json_file in *.json; do
     fi
 done
 
+---
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <project> <appname> <env>"
+    exit 1
+fi
+
+project=$1
+app=$2
+env=$3
+
+# Create JSON content
+json_content=$(cat <<EOF
+{
+    "project": "$project",
+    "appname": "$app",
+    "env": "$env",
+    "kustomize_template": "dotnet",
+    "org": "scl-$env"
+}
+EOF
+)
+
+# Create a directory to store the JSON files
+output_dir="json_output"
+mkdir -p "$output_dir"
+
+# Output JSON content to a file
+json_file="$output_dir/${app}_${env}.json"
+echo "$json_content" > "$json_file"
+echo "Created $json_file"
+
+# Make a POST request with the JSON content
+echo "Posting $json_file"
+curl -X POST -H "Content-Type: application/json" -d "$json_content" http://your-api-endpoint.com
+# Replace http://your-api-endpoint.com with the actual API endpoint
+
+# Add a newline for better readability
+echo "JSON file has been generated and posted."
